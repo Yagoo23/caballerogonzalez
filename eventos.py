8b033805740204d62d44bc190597c1402c2f1b3c
Yago CG <yagocg@correo.gal>
2021-10-05 12:51:02 +0200

    Módulo Clientes

---
+++ eventos.py
@@ -0,0 +1,13 @@
+'''
+Fichero de eventos generales
+'''
+import sys
+
+import var
+
+class Eventos():
+    def Salir(self):
+        try:
+            sys.exit()
+        except Exception as error:
+            print('Error en módulo salir ',error)
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-import sys
+import sys,var, eventos
 
 from window import *
 class Main(QtWidgets.QMainWindow):
     def __init__(self):
         super(Main,self).__init__()
-        self.ui=Ui_window()
-        self.ui.setupUi(self)
+        var.ui=Ui_window()
+        var.ui.setupUi(self)
+
+        '''
+        Eventos de botón
+        '''
+        var.ui.btnSalir.clicked.connect(eventos.Eventos.Salir)
+        '''
+        Eventos de la barra del menú
+        '''
+
+        var.ui.actionSalir.triggered.connect(eventos.Eventos.Salir)
 
 if __name__ == '__main__':
     app=QtWidgets.QApplication([])
