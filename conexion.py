7e5a545bdcf05235c7f370fa66d744aeb5b825ea
Yagoo23 <yagocg7@gmail.com>
2022-01-25 12:51:11 +0100

    Cargar tabla ventas 19/01

--- conexion.py
+++ conexion.py
@@ -512,6 +512,7 @@ class Conexion():
         try:
             var.ui.tabVentas.clearContents()
             index = 1
+
             #invoice.Facturas.cargaLineaVenta(0)
             query = QtSql.QSqlQuery()
             query.prepare('select codventa,precio,cantidad,codpro from ventas where codfac= :codfac')
